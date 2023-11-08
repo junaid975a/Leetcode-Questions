d2d102f9f0c6a17215041917a5e4c4d0e641138a
Junaid Ansari <junaidansari21975@gmail.com>
2023-11-08 11:11:34 +0530

    Time: 0 ms (100%), Space: 6.3 MB (5.3%) - LeetHub

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/3056-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/3056-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        if(sx == fx && sy == fy) return t != 1;
-        return (max(abs(sx - fx), abs(sy - fy)) <= t);
+        if(sx==fx && sy==fy) return t!=1;
+        int d = max(abs(fy-sy), abs(fx-sx));
+        // d = max({d, sy-fy,sx-fx});
+        // cout<<d<<t;
+        if(t>=d) return true;
+        return false;
     }
 };
